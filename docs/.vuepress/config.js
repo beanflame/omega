b8ed9ee3655c95ae82495fffa88a18bc42170bf1
bingling_sama <bingling_sama@qq.com>
2021-01-31 11:17:16 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    base: '/',
+    theme: 'antdocs',
+    host: 'localhost',
+    port: 10500,
+    title: 'Omega',
+    description: 'Minecraft 1.7.10 Forge Mod 开发手册! ',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ],
+    dest: '/docs/dist',
+    extraWatchFiles: [
+        '/docs/README.md',
+        '/docs/.vuepress/config.js'
+    ],
+    themeConfig: {
+        logo: '/icon.png',
+        nav: [
+            { text: 'Home', link: '/'},
+            { text: 'GitHub', link: '/'}
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+--- 
+home: true
+lang: zh-CN
+heroImage: /icon.png
+heroText: Omega 手册
+tagline: 教练，我想写 mod ！
+actionText: 开始入土 →
+actionLink: /guide/
+features:
+- title: 
+  details: 
+- title: 
+  details: 
+- title: 
+  details: 
+footer: Copyright © 2020-2021 Lollipop Studio. All rights reserved.
+---
